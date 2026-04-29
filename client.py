706f1513ff101abbc996f503eaba46ff5bf0455e
assem-ch <assemchebly.07@gmail.com>
2026-04-29 15:05:05 +0200

    Add generate message from the client class

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import random
+from message import Message
 
 class Client:
     _id_counter = 1
@@ -13,6 +14,9 @@ class Client:
 
     def get_client_id(self):
         return self._client_id
+
+    def generate_message(self):
+        return Message(source=self._client_id, destination=0, timestamp=self.get_next_interarrival_time())
 
     def print_client(self):
         print(f"Client ID: {self._client_id} | Lambda: {self._lambda} msgs/sec")
